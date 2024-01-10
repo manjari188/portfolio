96f634c802b2cec99ea9b2cf44ef37948a60c7b2
Manjari Dhoundiyal <manjari.dhoundiyal@hotmail.com>
2024-01-10 19:48:49 +0000

    added background

---
+++ src/Components/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div className="home-content container">
+            <div className="row">
+                <div className="col-md-7"></div>
+                <div className="col-md-5">
+                    <img style={{ position: 'relative', height: '75vh' }} src='/src/assets/Coding-amico.png'></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home
